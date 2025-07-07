b04a59e0e66a51e8885c087307ff7c199edbb3b2
annastriw <annastriwidagdoo@gmail.com>
2025-07-07 18:24:05 +0700

    fix: update useGetAllModules parameter

--- src/components/atoms/dialog/DialogCreateSubModuleDM.tsx
+++ src/components/atoms/dialog/DialogCreateSubModuleDM.tsx
@@ -77,9 +77,10 @@ export default function DialogCreateSubModuleDM({
   };
 
   const { data: session, status } = useSession();
-  const { data } = useGetAllModules(session?.access_token as string, {
-    enabled: status === "authenticated",
-  });
+  const { data } = useGetAllModules(session?.access_token as string, undefined, {
+  enabled: status === "authenticated",
+});
+
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
