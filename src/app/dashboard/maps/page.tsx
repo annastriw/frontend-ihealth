041921ecf000ab1954a1f8f6add4701bf6d22720
annastriw <129259825+annastriw@users.noreply.github.com>
2025-07-07 08:19:17 +0700

    Update page.tsx

--- src/app/dashboard/maps/page.tsx
+++ src/app/dashboard/maps/page.tsx
@@ -1,12 +1,5 @@
-'use client';
-
-import dynamic from "next/dynamic";
-
-const DashboardMapsWrapper = dynamic(
-  () => import("@/components/organisms/dashboard/maps/DashboardMapsWrapper"),
-  { ssr: false }
-);
+import ClientDashboardMaps from "./ClientDashboardMaps";
 
 export default function DashboardMapsPage() {
-  return <DashboardMapsWrapper />;
+  return <ClientDashboardMaps />;
 }
