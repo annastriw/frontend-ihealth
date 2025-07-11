fddc456a8fc99152c1086777f9be3094450f5735
annastriw <annastriwidagdoo@gmail.com>
2025-07-12 04:10:46 +0700

    feat: implement fetch and record of last opened time for module content

--- src/app/dashboard/modules/booklet/[id]/page.tsx
+++ src/app/dashboard/modules/booklet/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { api } from "@/lib/axios";
 import DashboardModulesBookletWrapper from "@/components/organisms/dashboard/booklet/DashboardModulesBookletWrapper";
 
 interface DashboardModulesBookletPageProps {
@@ -16,16 +15,6 @@ export default function DashboardModulesBookletPage({
   useEffect(() => {
     params.then(({ id }) => {
       setId(id);
-
-      // Panggil endpoint untuk update waktu buka
-      api
-        .post(`/module-content/${id}/opened`)
-        .then((res) => {
-          console.log("✅ Terakhir buka tercatat:", res.data);
-        })
-        .catch((err) => {
-          console.error("❌ Gagal update buka:", err);
-        });
     });
   }, [params]);
 
