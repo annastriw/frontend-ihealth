a32675490ee2c896b75e04ecac90430b9a9a65e6
annastriw <annastriwidagdoo@gmail.com>
2025-07-27 04:21:03 +0700

    feat: responsive layout for Simpan Booklet button in TabsContent

--- src/components/atoms/typography/DashboardTitleBold.tsx
+++ src/components/atoms/typography/DashboardTitleBold.tsx
@@ -4,7 +4,7 @@ interface DashboardTitleBoldProps {
 
 export default function DashboardTitleBold({ head }: DashboardTitleBoldProps) {
   return (
-    <div className="mb-7 w-full max-w-xl">
+    <div className="mb-7 w-full max-w-xl text-center lg:text-left mx-auto lg:mx-0">
       <h1 className="font-paytone text-3xl capitalize">{head}</h1>
     </div>
   );
