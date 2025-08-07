b4121b007302604f81bef80324a0be7b8dbd255e
unknown <alexbahrusy@gmail.com>
2025-08-07 07:48:03 +0700

    feat(alert): implement CSS fade-in and fade-out animations without Framer Motion

--- src/components/atoms/alert/AlertPersonalInformation.tsx
+++ src/components/atoms/alert/AlertPersonalInformation.tsx
@@ -3,13 +3,18 @@ import { TriangleAlert } from "lucide-react";
 
 export default function AlertPersonalInformation() {
   return (
-    <Alert variant={"warning"} className="mb-4 w-full md:w-fit">
-      <TriangleAlert className="!text-yellow-600" />
-      <AlertTitle>Belum Mengisi</AlertTitle>
-      <AlertDescription>
-        Anda belum mengisi informasi pribadi, silahkan isi terlebih dahulu
-        sebelum mengakses halaman utama.
-      </AlertDescription>
+    <Alert
+      variant="warning"
+      className="mb-4 w-full max-w-2xl border-2 md:w-fit"
+    >
+      <TriangleAlert className="mt-0.5 size-5 text-yellow-600" />
+      <div>
+        <AlertTitle>Informasi Pribadi Belum Diisi</AlertTitle>
+        <AlertDescription>
+          Anda belum mengisi informasi pribadi. Harap lengkapi terlebih dahulu
+          sebelum mengakses halaman utama.
+        </AlertDescription>
+      </div>
     </Alert>
   );
 }
