45fcaffce0615dc679fbb833d54a480637b953f0
unknown <alexbahrusy@gmail.com>
2025-08-11 21:13:09 +0700

    fix page dashboard/admin/reports/history/screening-dsmq/[id]

--- src/app/dashboard/medical/reports/history/screening-dsmq/[id]/page.tsx
+++ src/app/dashboard/medical/reports/history/screening-dsmq/[id]/page.tsx
@@ -1,18 +1,22 @@
-// src/app/dashboard/admin/reports/history/screening-dsmq/[id]/page.tsx
-
 import DashboardTitle from "@/components/atoms/typography/DashboardTitle";
-import DashboardAdminHistoryScreeningDSMQDetailWrapper from "@/components/organisms/dashboard/admin/reports/history/screening-dsmq/DashboardAdminHistoryScreeningDSMQDetailWrapper";
+import DashboardMedicalHistoryScreeningDSMQDetailWrapper from "@/components/organisms/dashboard/medical/reports/history/screening-dsmq/DashboardMedicalHistoryScreeningDSMQDetailWrapper";
 
-export default function Page({ params }: any) {
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export default function Page({ params }: PageProps) {
   const { id } = params;
 
   return (
     <section>
       <DashboardTitle
-        head="Detail Riwayat Screening DSMQ"
+        head="Detail Riwayat Screening DSMQ Medis"
         body="Menampilkan informasi lengkap mengenai riwayat screening DSMQ yang telah dilakukan oleh pasien diabetes. Halaman ini berisi detail hasil evaluasi manajemen diabetes secara menyeluruh untuk membantu pemantauan dan pengendalian kondisi kesehatan pasien."
       />
-      <DashboardAdminHistoryScreeningDSMQDetailWrapper id={id} />
+      <DashboardMedicalHistoryScreeningDSMQDetailWrapper id={id} />
     </section>
   );
 }
